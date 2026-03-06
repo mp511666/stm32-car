d602a1b94c56dafe46adfde91dcd324e7528acd7
mp511666 <647077527@qq.com>
2026-03-06 23:16:41 +0800

    fix:增加速度环

--- main/main.c
+++ main/main.c
@@ -22,7 +22,7 @@ uint16_t TimerCount;
 int16_t leftpwm,rightpwm;
 uint16_t avepwm,difpwm;      //全局变量定义输出的pwm（用于角度环）
 
-float leftspeeed,rightspeed; //输出的平均速度和差分速度（用于速度环）
+float leftspeed,rightspeed; //输出的平均速度和差分速度（用于速度环）
 float avespeed,difspeed;
 
 float AngleAcc;
@@ -33,14 +33,24 @@ uint8_t KeyNum,RunFlag;
 uint16_t Speed,PWM; //位置式pid
 
 pid_t anglepid={
-		.kp=0,
-		.ki=0,
-		.kd=0,
+		.kp=3,
+		.ki=0.1,
+		.kd=3,
 	
 		.outmax=100,
 		.outmin=-100,
 
 };   //角度环结构体，最先调整
+
+pid_t speedpid={
+		.kp=2.0,
+		.ki=0.05,
+		.kd=0,
+	
+		.outmax=20,
+		.outmin=-20,
+
+}; 
 
 int main(void)
 {
@@ -67,6 +77,7 @@ int main(void)
 			if(RunFlag==0)
 			{
 					pid_init(&anglepid);
+					pid_init(&speedpid);
 					RunFlag=1;
 			}
 			else
@@ -76,6 +87,7 @@ int main(void)
 		}
 		/*OLED显示*/
 		OLED_Clear();
+		/*显示angle环*/
 		OLED_Printf(0, 0, OLED_6X8, "angle:");
 		OLED_Printf(0, 8, OLED_6X8, "P:%05.2f",anglepid .kp);
 		OLED_Printf(0, 16, OLED_6X8, "I:%05.2f", anglepid.ki );
@@ -84,36 +96,58 @@ int main(void)
 		OLED_Printf(0, 40, OLED_6X8, "A:%+05.1f", Angle);
 		OLED_Printf(0, 48, OLED_6X8, "O:%+05.0f", anglepid .out);
 		OLED_Printf(0, 56, OLED_6X8, "GY:%+05d", GY);
+		/*显示speed环*/
+		OLED_Printf(50, 0,OLED_6X8, "speed");
+		OLED_Printf(50, 8, OLED_6X8, "%05.2f",speedpid .kp);
+		OLED_Printf(50, 16, OLED_6X8, "%05.2f", speedpid.ki );
+		OLED_Printf(50, 24, OLED_6X8, "%05.2f", speedpid .kd);
+		OLED_Printf(50, 32, OLED_6X8, "%+05.1f", speedpid .target);
+		OLED_Printf(50, 40, OLED_6X8, "%+05.1f", avespeed );
+		OLED_Printf(50, 48, OLED_6X8, "%+05.0f", speedpid .out);
 		OLED_Update();
 		
 		/*蓝牙串口数据，不理解*/
 		if(BlueSerial_RxFlag ==1)
 		{
 			char *Tag = strtok(BlueSerial_RxPacket, ",");	//提取数据1，定义为标签Tag
-//			if (strcmp(Tag, "key") == 0)					//Tag为key，收到按键数据包
-//			{
-//				char *Name = strtok(NULL, ",");				//提取数据2，定义为按键名称
-//				char *Action = strtok(NULL, ",");			//提取数据3，定义为按键动作
-//				
-//				/*此处可执行按键操作，目前程序暂时没用到按键，这段何意味。*/
-//			}
-			if (strcmp(Tag, "slider") == 0)			//Tag为slider，收到滑杆数据包
+			if (strcmp(Tag, "key") == 0)					//Tag为key，收到按键数据包
+			{
+				char *Name = strtok(NULL, ",");				//提取数据2，定义为按键名称
+				char *Action = strtok(NULL, ",");			//提取数据3，定义为按键动作
+				
+				/*此处可执行按键操作，目前程序暂时没用到按键，这段何意味。*/
+			}
+			else if (strcmp(Tag, "slider") == 0)			//Tag为slider，收到滑杆数据包
 			{
 				char *Name = strtok(NULL, ",");				//提取数据2，定义为滑杆名称
 				char *Value = strtok(NULL, ",");			//提取数据3，定义为滑杆值
 				
 				/*执行滑杆操作*/
-				if (strcmp(Name, "AngleKp") == 0)			//如果滑杆名称是AngleKp
+				/*调整angle环比例系数*/
+				if (strcmp(Name, "anglekp") == 0)			//如果滑杆名称是AngleKp
 				{
 					anglepid .kp = atof(Value);				//则把滑杆值赋值给角度环Kp
 				}
-				else if (strcmp(Name, "AngleKi") == 0)		//如果滑杆名称是AngleKi
+				else if (strcmp(Name, "angleki") == 0)		//如果滑杆名称是AngleKi
 				{
 					anglepid .ki = atof(Value);				//则把滑杆值赋值给角度环Ki
 				}
-				else if (strcmp(Name, "AngleKd") == 0)		//如果滑杆名称是AngleKd
+				else if (strcmp(Name, "anglekd") == 0)		//如果滑杆名称是AngleKd
 				{
 					anglepid .kd = atof(Value);				//则把滑杆值赋值给角度环Kd
+				}
+				/*调整speed环比例系数*/
+				if (strcmp(Name, "speedkp") == 0)			
+				{
+					speedpid  .kp = atof(Value);				
+				}
+				else if (strcmp(Name, "speedki") == 0)		
+				{
+					speedpid  .ki = atof(Value);				
+				}
+				else if (strcmp(Name, "speedkd") == 0)		
+				{
+					speedpid  .kd = atof(Value);				
 				}
 			}
 			else if (strcmp(Tag, "joystick") == 0)			//Tag为joystick，收到摇杆数据包
@@ -124,7 +158,7 @@ int main(void)
 				int8_t RV = atoi(strtok(NULL, ","));		//提取数据5，定义为摇杆值RV
 				
 				/*执行摇杆操作*/
-				anglepid .target  = LV / 10;		//摇杆值LV缩放后，控制角度环目标值，前后行进控制
+				speedpid .target  = LV / 25.0;		//摇杆值LV缩放后，控制角度环目标值，前后行进控制
 				difpwm  = RH / 2;				//摇杆值RH缩放后，控制差分PWM，左右转弯控制
 			}
 			
@@ -132,7 +166,7 @@ int main(void)
 		}
 		
 		/*蓝牙串口打印波形，需配合蓝牙串口小程序实现波形绘制*/
-		BlueSerial_Printf("[plot,%f,%f]", anglepid .target, Angle);	//绘制角度环的波形
+		BlueSerial_Printf("[plot,%f,%f]", anglepid .target, Angle);	//绘制角度环的波形(随调整的环进行改变)
 	}
 		
 }
@@ -177,6 +211,7 @@ void TIM1_UP_IRQHandler(void)
 				leftpwm =avepwm +difpwm/2;
 				rightpwm=avepwm-difpwm/2;
 				
+				
 				if(leftpwm>100){leftpwm =100;}else if(leftpwm<-100){leftpwm =-100;}
 				if(rightpwm >100){rightpwm =100;}else if(rightpwm<-100){rightpwm =-100;}
 				
@@ -189,6 +224,24 @@ void TIM1_UP_IRQHandler(void)
 				Motor_SetPWM (1,0);
 				Motor_SetPWM (2,0);
 			}
+		}
+		
+		Count0 ++;
+		if(Count0 >=50)   //读取编码器的时间间隔：50ms
+		{
+				Count0=0;
+				leftspeed =Encoder_Get (1)/44.0/0.05/9.27666;
+				rightspeed =Encoder_Get (1)/44.0/0.05/9.27666; //转换公式
+			
+				avespeed=(leftspeed+rightspeed)/2.0;
+				difspeed =leftspeed -rightspeed ;
+			
+				if(RunFlag)
+				{
+						speedpid .actual =avespeed;
+						pid_update(&speedpid );
+						anglepid.target=speedpid.out;
+				}
 		}
 		
 		
